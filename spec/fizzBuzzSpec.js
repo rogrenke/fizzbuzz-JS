8ac7d93df5f374deb3648852a10f4aa3aaa7eda7
“rogrenke” <roland@rolandgrenke.com>
2017-06-06 12:06:05 +0100

    creates functionality to return 'fizzbuzz' if multiple of 3&5, minor refactoring of Jasmine test structure

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,26 +1,45 @@
 describe("FizzBuzz", function() {
-	it("should print 'fizz' for 3", function() {
-		expect(FizzBuzz(3)).toEqual("fizz");
+	
+	describe("multiples of 3", function() {
+		it("should print 'fizz' for 3", function() {
+			expect(FizzBuzz(3)).toEqual("fizz");
+		});
+
+		it("should print 'fizz' for 39", function() {
+			expect(FizzBuzz(39)).toEqual("fizz");
+		});
+
+		it("should print 'fizz' for 93", function() {
+			expect(FizzBuzz(93)).toEqual("fizz");
+		});
 	});
 
-	it("should print 'fizz' for 39", function() {
-		expect(FizzBuzz(39)).toEqual("fizz");
+	describe("multiples of 5", function() {
+		it("should print 'buzz' for 5", function() {
+			expect(FizzBuzz(5)).toEqual("buzz");
+		});
+
+		it("should print 'buzz' for 35", function() {
+			expect(FizzBuzz(35)).toEqual("buzz");
+		});
+
+		it("should print 'buzz' for 95", function() {
+			expect(FizzBuzz(95)).toEqual("buzz");
+		});
 	});
 
-	it("should print 'fizz' for 93", function() {
-		expect(FizzBuzz(93)).toEqual("fizz");
-	});
+	describe("multiples of 3&5", function() {
+		it("should print 'fizzbuzz' for 15", function() {
+			expect(FizzBuzz(15)).toEqual("fizzbuzz");
+		});
 
-	it("should print 'buzz' for 5", function() {
-		expect(FizzBuzz(5)).toEqual("buzz");
-	});
+		it("should print 'fizzbuzz' for 45", function() {
+			expect(FizzBuzz(45)).toEqual("fizzbuzz");
+		});
 
-	it("should print 'buzz' for 35", function() {
-		expect(FizzBuzz(35)).toEqual("buzz");
-	});
-
-	it("should print 'buzz' for 95", function() {
-		expect(FizzBuzz(95)).toEqual("buzz");
+		it("should print 'fizzbuzz' for 90", function() {
+			expect(FizzBuzz(90)).toEqual("fizzbuzz");
+		});
 	});
 });
 
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,8 +1,10 @@
 var FizzBuzz = function(number) {
-	if (number % 3 === 0) {
-		return "fizz"
+	if (number % 3 === 0 && number % 5 === 0) {
+		return "fizzbuzz"
 	} else if (number % 5 == 0) {
 		return "buzz"
+	} else if (number % 3 == 0) {
+		return "fizz"
 	} else {
 		return number
 	};
