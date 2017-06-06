f899c060e194ba8830ad460e573cef78ddd928b8
“rogrenke” <roland@rolandgrenke.com>
2017-06-06 11:50:11 +0100

    Adds functionality to return 'buzz' for multiples of 5

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -2,6 +2,26 @@ describe("FizzBuzz", function() {
 	it("should print 'fizz' for 3", function() {
 		expect(FizzBuzz(3)).toEqual("fizz");
 	});
+
+	it("should print 'fizz' for 39", function() {
+		expect(FizzBuzz(39)).toEqual("fizz");
+	});
+
+	it("should print 'fizz' for 93", function() {
+		expect(FizzBuzz(93)).toEqual("fizz");
+	});
+
+	it("should print 'buzz' for 5", function() {
+		expect(FizzBuzz(5)).toEqual("buzz");
+	});
+
+	it("should print 'buzz' for 35", function() {
+		expect(FizzBuzz(35)).toEqual("buzz");
+	});
+
+	it("should print 'buzz' for 95", function() {
+		expect(FizzBuzz(95)).toEqual("buzz");
+	});
 });
 
 // describe("FizzBuzz", function() {
--- src/fizzBuzz.js
+++ src/fizzBuzz.js
@@ -1,6 +1,8 @@
 var FizzBuzz = function(number) {
 	if (number % 3 === 0) {
 		return "fizz"
+	} else if (number % 5 == 0) {
+		return "buzz"
 	} else {
 		return number
 	};
