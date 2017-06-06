12eee8f0953925c77063531979976333318bde56
“rogrenke” <roland@rolandgrenke.com>
2017-06-06 12:10:18 +0100

    adds tests for all other numbers, they pass

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -1,44 +1,58 @@
 describe("FizzBuzz", function() {
 	
 	describe("multiples of 3", function() {
-		it("should print 'fizz' for 3", function() {
+		it("should return 'fizz' for 3", function() {
 			expect(FizzBuzz(3)).toEqual("fizz");
 		});
 
-		it("should print 'fizz' for 39", function() {
+		it("should return 'fizz' for 39", function() {
 			expect(FizzBuzz(39)).toEqual("fizz");
 		});
 
-		it("should print 'fizz' for 93", function() {
+		it("should return 'fizz' for 93", function() {
 			expect(FizzBuzz(93)).toEqual("fizz");
 		});
 	});
 
 	describe("multiples of 5", function() {
-		it("should print 'buzz' for 5", function() {
+		it("should return 'buzz' for 5", function() {
 			expect(FizzBuzz(5)).toEqual("buzz");
 		});
 
-		it("should print 'buzz' for 35", function() {
+		it("should return 'buzz' for 35", function() {
 			expect(FizzBuzz(35)).toEqual("buzz");
 		});
 
-		it("should print 'buzz' for 95", function() {
+		it("should return 'buzz' for 95", function() {
 			expect(FizzBuzz(95)).toEqual("buzz");
 		});
 	});
 
 	describe("multiples of 3&5", function() {
-		it("should print 'fizzbuzz' for 15", function() {
+		it("should return 'fizzbuzz' for 15", function() {
 			expect(FizzBuzz(15)).toEqual("fizzbuzz");
 		});
 
-		it("should print 'fizzbuzz' for 45", function() {
+		it("should return 'fizzbuzz' for 45", function() {
 			expect(FizzBuzz(45)).toEqual("fizzbuzz");
 		});
 
-		it("should print 'fizzbuzz' for 90", function() {
+		it("should return 'fizzbuzz' for 90", function() {
 			expect(FizzBuzz(90)).toEqual("fizzbuzz");
+		});
+	});
+
+	describe("all other numbers", function() {
+		it("should return 7 for 7", function() {
+			expect(FizzBuzz(7)).toEqual(7);
+		});
+
+		it("should return 22 for 22", function() {
+			expect(FizzBuzz(22)).toEqual(22);
+		});
+
+		it("should return 64 for 64", function() {
+			expect(FizzBuzz(64)).toEqual(64);
 		});
 	});
 });
